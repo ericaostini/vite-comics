60c8cec8a0a335be0e6e74addf7f4b52c0e023d2
ericaostini <ericaostini14@gmail.com>
2023-11-09 17:06:31 +0100

    create object array for footer info "footerInfos"

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,14 +1,40 @@
 <template>
     <footer>
         <section class="footer-top">
-
+            <ul v-for="(info, index) in footerInfos" :key="index">
+                <li>{{ info.title }}</li>
+                <li v-for="(i, index) in info.inform">{{ i }}</li>
+            </ul>
         </section>
     </footer>
 </template>
 
 <script>
 export default {
-    name: 'FooterComponents'
+    name: 'FooterComponents',
+    data() {
+        return {
+            footerInfos: [
+                {
+                    title: 'DC COMICS',
+                    inform: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+                },
+                {
+                    title: 'DC',
+                    inform: ['Terms Of Use', 'Privacy policy', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
+                },
+                {
+                    title: 'SITES',
+                    inform: ['DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+                },
+                {
+                    title: 'SHOP',
+                    inform: ['Shop DC', 'Shop DC Collectibles']
+                },
+
+            ]
+        }
+    },
 }
 </script>
 
@@ -16,6 +42,11 @@ export default {
 footer {
     .footer-top {
         background-image: url("../images/footer-bg.jpg");
+        height: 300px;
+
+        li {
+            color: white;
+        }
     }
 }
 </style>
