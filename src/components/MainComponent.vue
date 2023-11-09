5471c57e80e5ef1a8fc414d7458fc6114631423e
ericaostini <ericaostini14@gmail.com>
2023-11-09 16:05:46 +0100

    style section-one

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
-        Main
+        <section class="section-one">
+            <p class="container">--Content goes here--</p>
+        </section>
     </main>
 </template>
 
@@ -10,4 +12,21 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+main {
+    .section-one {
+        height: 120px;
+        background-color: black;
+        position: relative;
+
+        p {
+            color: white;
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+}
+</style>
