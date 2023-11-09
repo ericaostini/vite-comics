0e41a18749c96813d22162f2a4e2fce4163dd748
ericaostini <ericaostini14@gmail.com>
2023-11-09 15:32:35 +0100

    nav bar creation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,68 @@
 <template>
-    <nav class="container">
-        nav bar
+    <nav class="nav">
+        <a class="nav-link text-light" :class="{ 'active': info.active }" href="#" v-for="(info, index) in informations"
+            :key="index">{{ info.text }}</a>
     </nav>
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    data() {
+        return {
+            informations: [
+                {
+                    text: 'CHARACTERS',
+                    active: false,
+                },
+                {
+                    text: 'COMICS',
+                    active: true,
+                },
+                {
+                    text: 'MOVIES',
+                    active: false,
+                },
+                {
+                    text: 'TV',
+                    active: false,
+                },
+                {
+                    text: 'GAMES',
+                    active: false,
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    active: false,
+                },
+                {
+                    text: 'VIDEOS',
+                    active: false,
+                },
+                {
+                    text: 'FANS',
+                    active: false,
+                },
+                {
+                    text: 'NEWS',
+                    active: false,
+                },
+                {
+                    text: 'SHOP',
+                    active: false,
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 nav {
     color: white;
+}
+
+.active {
+    background-color: yellow;
 }
 </style>
