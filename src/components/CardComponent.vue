a390f56eecdd14f92871f5f6cb02f8d7732cb25f
ericaostini <ericaostini14@gmail.com>
2023-11-10 15:04:49 +0100

    create props and templace in cardComponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
-
+    <div class="col-4">
+        <img :src="image" alt="serie">
+        <p><strong>{{ serie }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'CardComponent'
+    name: 'CardComponent',
+    props: ["image", "serie"]
 }
 </script>
 
